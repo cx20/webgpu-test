a1294a6e413c0f12c9630380d2e69e9cdfed03d4
cx20 <y-shimano@hondarer-soft.com>
2020-02-19 04:43:48 +0900

    Added three.js examples

---
+++ examples/threejs/texture/index.js
@@ -0,0 +1,66 @@
+import {
+    BoxBufferGeometry,
+    Camera,
+    Mesh,
+    MeshBasicMaterial,
+    PerspectiveCamera,
+    Scene,
+    Texture,
+    TextureLoader
+} from 'https://raw.githack.com/mrdoob/three.js/r111/build/three.module.js';
+import WebGPURenderer from 'https://rawcdn.githack.com/takahirox/THREE.WebGPURenderer/44d91fcc5ce2f92d71f1811d36f59b5a6510753e/src/renderers/WebGPURenderer.js';
+import glslangModule from 'https://rawcdn.githack.com/takahirox/THREE.WebGPURenderer/44d91fcc5ce2f92d71f1811d36f59b5a6510753e/examples/jsm/libs/glslang.js';
+
+const run = async () => {
+    const adapter = await navigator.gpu.requestAdapter();
+    const device = await adapter.requestDevice();
+    const glslang = await glslangModule();
+
+    const renderer = new WebGPURenderer({
+        device,
+        glslang
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    document.body.appendChild(renderer.domElement);
+
+    const scene = new Scene();
+
+    const camera = new PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000.0);
+    camera.position.z = 5.0;
+
+    let box;
+
+    new TextureLoader().load('../../../assets/textures/frog.jpg', texture => {
+        box = new Mesh(
+            new BoxBufferGeometry(1, 1, 1),
+            new MeshBasicMaterial({
+                map: texture
+            })
+        );
+        box.rotation.x = 45 * Math.PI / 180;
+        scene.add(box);
+
+    });
+
+    const render = () => {
+        requestAnimationFrame(render);
+        if (box) {
+            box.rotation.x += 0.01;
+            box.rotation.y += 0.01;
+        }
+        renderer.render(scene, camera);
+    };
+
+    const onResize = event => {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    };
+
+    window.addEventListener('resize', onResize, false);
+
+    render();
+};
+
+run();
