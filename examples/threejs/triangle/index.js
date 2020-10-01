86198053a2fb2965a082f3bebb7c13267d8b885b
cx20 <y-shimano@hondarer-soft.com>
2020-10-02 02:57:28 +0900

    Added NOTE to Triangle sample in Three.js

--- examples/threejs/triangle/index.js
+++ examples/threejs/triangle/index.js
@@ -1,6 +1,51 @@
 import * as THREE from 'https://raw.githack.com/mrdoob/three.js/r121/build/three.module.js';
 import WebGPURenderer from 'https://raw.githack.com/mrdoob/three.js/r121/examples/jsm/renderers/webgpu/WebGPURenderer.js';
 
+// NOTE: The shader currently used in the WebGPU Renderer's MeshBasicMaterial is the following code.
+//       Please note that you cannot specify the color because you are using a texture.
+
+// https://github.com/mrdoob/three.js/blob/dev/examples/jsm/renderers/webgpu/WebGPURenderPipelines.js#L781-L802
+// #version 450
+// 
+// layout(location = 0) in vec3 position;
+// layout(location = 1) in vec2 uv;
+// 
+// layout(location = 0) out vec2 vUv;
+// 
+// layout(set = 0, binding = 0) uniform ModelUniforms {
+//     mat4 modelMatrix;
+//     mat4 modelViewMatrix;
+//     mat3 normalMatrix;
+// } modelUniforms;
+// 
+// layout(set = 0, binding = 1) uniform CameraUniforms {
+//     mat4 projectionMatrix;
+//     mat4 viewMatrix;
+// } cameraUniforms;
+// 
+// void main(){
+//     vUv = uv;
+//     gl_Position = cameraUniforms.projectionMatrix * modelUniforms.modelViewMatrix * vec4( position, 1.0 );
+// }
+// 
+
+// https://github.com/mrdoob/three.js/blob/dev/examples/jsm/renderers/webgpu/WebGPURenderPipelines.js#L803-L817
+// #version 450
+// layout(set = 0, binding = 2) uniform OpacityUniforms {
+//     float opacity;
+// } opacityUniforms;
+// 
+// layout(set = 0, binding = 3) uniform sampler mySampler;
+// layout(set = 0, binding = 4) uniform texture2D myTexture;
+// 
+// layout(location = 0) in vec2 vUv;
+// layout(location = 0) out vec4 outColor;
+// 
+// void main() {
+//     outColor = texture( sampler2D( myTexture, mySampler ), vUv );
+//     outColor.a *= opacityUniforms.opacity;
+// }
+
 let camera, scene, renderer;
 
 init().then( animate ).catch( error );
