ad2755ea0c101421287079bebfd03eaed68e4df9
cx20 <y-shimano@hondarer-soft.com>
2020-10-04 06:23:43 +0900

    Added Three.js WebGPU samples

--- examples/threejs/texture/index.js
+++ examples/threejs/texture/index.js
@@ -1,62 +1,192 @@
-import {
-    BoxBufferGeometry,
-    Camera,
-    Mesh,
-    MeshBasicMaterial,
-    PerspectiveCamera,
-    Scene,
-    Texture,
-    TextureLoader
-} from 'https://raw.githack.com/mrdoob/three.js/r111/build/three.module.js';
-import WebGPURenderer from 'https://rawcdn.githack.com/takahirox/THREE.WebGPURenderer/a2f57312bf9968fa1c415d63d46b0b35a8c9897f/src/renderers/WebGPURenderer.js';
-import glslangModule from 'https://rawcdn.githack.com/takahirox/THREE.WebGPURenderer/a2f57312bf9968fa1c415d63d46b0b35a8c9897f/examples/jsm/libs/glslang.js';
+import * as THREE from 'https://raw.githack.com/mrdoob/three.js/r121/build/three.module.js';
+import WebGPURenderer from 'https://raw.githack.com/mrdoob/three.js/r121/examples/jsm/renderers/webgpu/WebGPURenderer.js';
 
-const run = async () => {
-    const adapter = await navigator.gpu.requestAdapter();
-    const device = await adapter.requestDevice();
-    const glslang = await glslangModule();
+// NOTE: The shader currently used in the WebGPU Renderer's MeshBasicMaterial is the following code.
+//       Please note that you cannot specify the color because you are using a texture.
 
-    const renderer = new WebGPURenderer({
-        device,
-        glslang
-    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    document.body.appendChild(renderer.domElement);
+// https://github.com/mrdoob/three.js/blob/dev/examples/jsm/renderers/webgpu/WebGPURenderPipelines.js#L781-L802
+// #version 450
+// 
+// layout(location = 0) in vec3 position;
+// layout(location = 1) in vec2 uv;
+// 
+// layout(location = 0) out vec2 vUv;
+// 
+// layout(set = 0, binding = 0) uniform ModelUniforms {
+//     mat4 modelMatrix;
+//     mat4 modelViewMatrix;
+//     mat3 normalMatrix;
+// } modelUniforms;
+// 
+// layout(set = 0, binding = 1) uniform CameraUniforms {
+//     mat4 projectionMatrix;
+//     mat4 viewMatrix;
+// } cameraUniforms;
+// 
+// void main(){
+//     vUv = uv;
+//     gl_Position = cameraUniforms.projectionMatrix * modelUniforms.modelViewMatrix * vec4( position, 1.0 );
+// }
+// 
 
-    const scene = new Scene();
+// https://github.com/mrdoob/three.js/blob/dev/examples/jsm/renderers/webgpu/WebGPURenderPipelines.js#L803-L817
+// #version 450
+// layout(set = 0, binding = 2) uniform OpacityUniforms {
+//     float opacity;
+// } opacityUniforms;
+// 
+// layout(set = 0, binding = 3) uniform sampler mySampler;
+// layout(set = 0, binding = 4) uniform texture2D myTexture;
+// 
+// layout(location = 0) in vec2 vUv;
+// layout(location = 0) out vec4 outColor;
+// 
+// void main() {
+//     outColor = texture( sampler2D( myTexture, mySampler ), vUv );
+//     outColor.a *= opacityUniforms.opacity;
+// }
 
-    const camera = new PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000.0);
+let camera, scene, renderer, mesh;
+
+init().then( animate ).catch( error );
+
+async function init() {
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000.0);
     camera.position.z = 5.0;
 
-    let box;
-    const loader = new TextureLoader();
-    loader.load('../../../assets/textures/frog.jpg', texture => {
-        const geometry = new BoxBufferGeometry(1, 1, 1);
-        const material = new MeshBasicMaterial({map: texture});
-        box = new Mesh(geometry, material);
-        scene.add(box);
 
+    // Cube data
+    //             1.0 y 
+    //              ^  -1.0 
+    //              | / z
+    //              |/       x
+    // -1.0 -----------------> +1.0
+    //            / |
+    //      +1.0 /  |
+    //           -1.0
+    // 
+    //         [7]------[6]
+    //        / |      / |
+    //      [3]------[2] |
+    //       |  |     |  |
+    //       | [4]----|-[5]
+    //       |/       |/
+    //      [0]------[1]
+    //
+    const vertices = [
+            // Front face
+            -0.5, -0.5,  0.5, // v0
+             0.5, -0.5,  0.5, // v1
+             0.5,  0.5,  0.5, // v2
+            -0.5,  0.5,  0.5, // v3
+            // Back face
+            -0.5, -0.5, -0.5, // v4
+             0.5, -0.5, -0.5, // v5
+             0.5,  0.5, -0.5, // v6
+            -0.5,  0.5, -0.5, // v7
+            // Top face
+             0.5,  0.5,  0.5, // v2
+            -0.5,  0.5,  0.5, // v3
+            -0.5,  0.5, -0.5, // v7
+             0.5,  0.5, -0.5, // v6
+            // Bottom face
+            -0.5, -0.5,  0.5, // v0
+             0.5, -0.5,  0.5, // v1
+             0.5, -0.5, -0.5, // v5
+            -0.5, -0.5, -0.5, // v4
+            // Right face
+             0.5, -0.5,  0.5, // v1
+             0.5,  0.5,  0.5, // v2
+             0.5,  0.5, -0.5, // v6
+             0.5, -0.5, -0.5, // v5
+            // Left face
+            -0.5, -0.5,  0.5, // v0
+            -0.5,  0.5,  0.5, // v3
+            -0.5,  0.5, -0.5, // v7
+            -0.5, -0.5, -0.5  // v4
+    ];
+    const uvs = [
+        // Front face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Back face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Top face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Bottom face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Right face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
+        // Left face
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1
+    ];
+    const indices = [
+         0,  1,  2,    0,  2 , 3,  // Front face
+         4,  5,  6,    4,  6 , 7,  // Back face
+         8,  9, 10,    8, 10, 11,  // Top face
+        12, 13, 14,   12, 14, 15,  // Bottom face
+        16, 17, 18,   16, 18, 19,  // Right face
+        20, 21, 22,   20, 22, 23   // Left face
+    ];
+    
+    const geometry = new THREE.BufferGeometry();
+    geometry.setIndex(indices);
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    geometry.setAttribute('normal', new THREE.Float32BufferAttribute(vertices, 3));
+    geometry.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2)); // TODO: If you do not specify the uv attribute, an error occurs
+
+    const loader = new THREE.TextureLoader();
+    loader.load('../../../assets/textures/frog.jpg', texture => {
+        const material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
+        mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
     });
 
-    const render = () => {
-        requestAnimationFrame(render);
-        if (box) {
-            box.rotation.x += 0.01;
-            box.rotation.y += 0.01;
-        }
-        renderer.render(scene, camera);
-    };
+    renderer = new WebGPURenderer();
+    renderer.setClearColor(0xffffff);
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-    const onResize = event => {
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-    };
+    window.addEventListener( 'resize', onWindowResize, false );
 
-    window.addEventListener('resize', onResize, false);
+    return renderer.init();
+}
 
-    render();
-};
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-run();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+    if ( mesh ) {
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.01;
+    }
+    renderer.render( scene, camera );
+}
+
+function error( error ) {
+    console.error( error );
+}
