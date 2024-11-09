5d8b64bfe0a4d89e1ff2e2f6dce9c2b2acac88fb
cx20 <y-shimano@hondarer-soft.com>
2024-11-09 14:09:04 +0900

    Updated Three.js WebGPU samples to latest

--- examples/threejs/cube/index.js
+++ examples/threejs/cube/index.js
@@ -1,51 +1,5 @@
 import * as THREE from 'three';
-import WebGPURenderer from 'three/addons/renderers/webgpu/WebGPURenderer.js';
-import * as Nodes from 'three/nodes';
-
-// NOTE: The shader currently used in the WebGPU Renderer's MeshBasicMaterial is the following code.
-//       Please note that you cannot specify the color because you are using a texture.
-
-// https://github.com/mrdoob/three.js/blob/dev/examples/jsm/renderers/webgpu/WebGPURenderPipelines.js#L781-L802
-// #version 450
-// 
-// layout(location = 0) in vec3 position;
-// layout(location = 1) in vec2 uv;
-// 
-// layout(location = 0) out vec2 vUv;
-// 
-// layout(set = 0, binding = 0) uniform ModelUniforms {
-//     mat4 modelMatrix;
-//     mat4 modelViewMatrix;
-//     mat3 normalMatrix;
-// } modelUniforms;
-// 
-// layout(set = 0, binding = 1) uniform CameraUniforms {
-//     mat4 projectionMatrix;
-//     mat4 viewMatrix;
-// } cameraUniforms;
-// 
-// void main(){
-//     vUv = uv;
-//     gl_Position = cameraUniforms.projectionMatrix * modelUniforms.modelViewMatrix * vec4( position, 1.0 );
-// }
-// 
-
-// https://github.com/mrdoob/three.js/blob/dev/examples/jsm/renderers/webgpu/WebGPURenderPipelines.js#L803-L817
-// #version 450
-// layout(set = 0, binding = 2) uniform OpacityUniforms {
-//     float opacity;
-// } opacityUniforms;
-// 
-// layout(set = 0, binding = 3) uniform sampler mySampler;
-// layout(set = 0, binding = 4) uniform texture2D myTexture;
-// 
-// layout(location = 0) in vec2 vUv;
-// layout(location = 0) out vec4 outColor;
-// 
-// void main() {
-//     outColor = texture( sampler2D( myTexture, mySampler ), vUv );
-//     outColor.a *= opacityUniforms.opacity;
-// }
+import { color, uv, attribute } from 'three/tsl';
 
 let camera, scene, renderer, mesh;
 
@@ -108,38 +62,21 @@ async function init() {
             -0.5,  0.5, -0.5, // v7
             -0.5, -0.5, -0.5  // v4
     ];
-    const uvs = [
-        // Front face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        // Back face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        // Top face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        // Bottom face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        // Right face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        // Left face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1
+    const colors = [
+        [1.0, 0.0, 0.0, 1.0], // Front face
+        [1.0, 1.0, 0.0, 1.0], // Back face
+        [0.0, 1.0, 0.0, 1.0], // Top face
+        [1.0, 0.5, 0.5, 1.0], // Bottom face
+        [1.0, 0.0, 1.0, 1.0], // Right face
+        [0.0, 0.0, 1.0, 1.0]  // Left face
     ];
+    let unpackedColors = [];
+    for (let i in colors) {
+        let color = colors[i];
+        for (let j=0; j < 4; j++) {
+            unpackedColors = unpackedColors.concat(color);
+        }
+    }
     const indices = [
          0,  1,  2,    0,  2 , 3,  // Front face
          4,  5,  6,    4,  6 , 7,  // Back face
@@ -152,16 +89,15 @@ async function init() {
     const geometry = new THREE.BufferGeometry();
     geometry.setIndex(indices);
     geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-    geometry.setAttribute('normal', new THREE.Float32BufferAttribute(vertices, 3));
-    geometry.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2)); // TODO: If you do not specify the uv attribute, an error occurs
+    geometry.setAttribute('color', new THREE.Float32BufferAttribute(unpackedColors, 4));
 
-    const material = new Nodes.MeshBasicNodeMaterial({ side: THREE.DoubleSide });
-    material.colorNode = new Nodes.PositionNode( Nodes.PositionNode.LOCAL );
-
+    const material = new THREE.MeshBasicNodeMaterial({ side: THREE.DoubleSide });
+    material.colorNode = attribute('color');
+    
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
-    renderer = new WebGPURenderer();
+    renderer = new THREE.WebGPURenderer();
     renderer.setClearColor(0xffffff);
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
